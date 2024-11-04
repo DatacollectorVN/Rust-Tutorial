33693efc66c59e318a892c5c38de0ecad3c3ff84
DatacollectorVN <ntrong@jsr-nahq.com>
2024-11-04 15:21:16 +0700

    [Feature] Added helloworl tutorial

---
+++ labs/hello_world/main.rs
@@ -0,0 +1,7 @@
+// ref: https://doc.rust-lang.org/book/ch01-02-hello-world.html
+
+fn main() {
+    // using a ! means that you’re calling a macro instead of a normal function and that macros don’t
+    // always follow the same rules as functions.
+    println!("Hello world");
+}
